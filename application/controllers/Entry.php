c1ebdfa30a0f1fbcdc6870f931d45b073856d024
cjsysdev <cjsysdev@gmail.com>
2024-08-05 10:07:07 +0800

    signup n student_info 2

--- application/controllers/Entry.php
+++ application/controllers/Entry.php
@@ -3,14 +3,45 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Entry extends CI_Controller {
 
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('accounts');  
+        $this->load->helper(['url_helper']);      
+    }
+
 	public function index()
 	{
 		$this->load->view('login');
 	}
 
+    public function signup(){
+        $this->load->view('signup');
+    }
+
+    public function student_info(){
+        $this->load->view('student_info');
+    }
+
+    public function home(){
+        $this->load->view('home');
+    }
+
+    public function admin(){
+        $this->load->view('admin');
+    }
+
     public function login(){
-        echo 'login';
-        var_dump($_POST);
+        $post = $this->input->post();
+        $user = $this->accounts->get(['username' => $post['username']]);
+
+        if($user && $user->password == $post['password']){
+            echo 'login true';
+        } else {
+            echo 'asdf';
+        }
+
+        var_dump($user, $post);
     }
 
 }
---
+++ application/models/accounts.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class accounts extends MY_Model
+{
+    public $table = 'accounts';
+    public $primary_key = 'account_no';
+    public $protected = array('account_no');
+
+    public function __construct()
+    {
+        $this->timestamps = FALSE;
+        parent::__construct();
+    }
+    
+}
